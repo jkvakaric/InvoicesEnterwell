ca313d21b280496682228665ab6cf92566347d6c
Josip Kvakarić <josip.kvakaric@outlook.com>
2017-02-06 23:38:21 +0100

    models

---
+++ IzdavanjeFaktura/Models/Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IzdavanjeFaktura.Models
+{
+    public class Invoice
+    {
+        public Guid Id { get; set; }
+        [DisplayName("Invoice Number")]
+        public string Number { get; set; }
+        [DisplayName("Date Created")]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+        [DisplayName("Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime DueDateTime { get; set; }
+        [DisplayName("No VAT Price")]
+        [DataType(DataType.Currency)]
+        public double NoVatPrice { get; set; }
+        [DisplayName("VAT Country")]
+        public VatType Vat { get; set; }
+        [DisplayName("Full Price")]
+        [DataType(DataType.Currency)]
+        public double FullPrice { get; set; }
+        [DisplayName("Bill To")]
+        public string Receiver { get; set; }
+
+        public List<InvoiceItem> InvoiceItems { get; set; }
+        public virtual ApplicationUser Creator { get; set; }
+
+        public Invoice(){ }
+
+        public enum VatType
+        {
+            Croatian, Bosnian, Slovenian
+        }
+    }
+}
---
+++ IzdavanjeFaktura/Models/InvoiceItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IzdavanjeFaktura.Models
+{
+    public class InvoiceItem
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayName("NoVAT Item Price")]
+        public double NoVatItemPrice { get; set; }
+        public int Quantity { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayName("Total NoVAT Price")]
+        public double TotalNoVatPrice { get; set; }
+
+        public virtual Invoice Invoice { get; set; }
+
+        public InvoiceItem() { }
+    }
+}
